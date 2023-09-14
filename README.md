57028b11bd5371575f6259a0c0d0f579d5867f07
incihuseynli <inci.huseynli0@gmail.com>
2023-09-14 06:44:05 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dad-Jokes-Generetor
+ This is a simple JS API - Jokes Generetor challenge from 50projects50days.comhttps://50projects50days.com/#projects
---
+++ main.js
@@ -0,0 +1,21 @@
+let options = {
+  method: "GET",
+  headers: { "X-Api-Key": "rWpBwJzunYSJv7tTCqBDXA==g48LVHZEVAsnC45y" },
+};
+
+let url = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+const container = document.querySelector(".container");
+fetch(url, options)
+  .then((res) => res.json())
+  .then((data) => {
+    container.innerHTML += `
+          <span class="title">#Don't Laugh Challenge</span>
+          <p class="joke">${data[0].joke}</p>
+          <button class="btn" onclick="getNewJoke()">Get Another Joke</button>
+          `;
+  });
+
+function getNewJoke() {
+  window.location.reload();
+}
